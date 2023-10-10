225b4434a62f09c269eec4f8602e698a124945a3
reqsanemustafayevaa <146015080+reqsanemustafayevaa@users.noreply.github.com>
2023-10-10 11:50:45 +0400

    Add project files.

---
+++ Human.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capsulation_task
+{
+    public class Human
+    {
+        public string FullName { get; set; }
+        public double PhoneNumber { get; set; }
+        private byte _age;
+
+        public byte Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value > 0 && value < 150)
+                {
+                    _age = value;
+                }
+            }
+        }
+            public string Name (string word1,string word2)
+        {
+            return word1 + word2;
+        }
+        
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capsulation_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+            human.FullName = "Raghsana";
+            human.PhoneNumber = 706281112;
+            human.Age = 176;
+            Console.WriteLine(human.Age);
+
+           
+            
+           
+           
+        }
+      
+        
+    }
+}
